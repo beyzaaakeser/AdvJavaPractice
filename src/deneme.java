52f10d77e282623af10a3c00be534055e9e9039c
Beyza KESER <beyzaaakeser@gmail.com>
2022-12-24 00:31:19 +0300

    v1.1

--- src/deneme.java
+++ src/deneme.java
@@ -1,10 +1,21 @@
-public class deneme {
+public class deneme <T> {
+
 
     public static void main(String[] args) {
 
         System.out.println("Generic yapilar");
-
+        sum(25);
     }
 
+    public static void sum(int t){
+        if(t>0){
+            System.out.print(t + " ");
+            t=t-1;
+            sum(t);
+        }
+
+
+
+    }
 
 }
